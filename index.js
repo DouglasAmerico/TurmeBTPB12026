981019c3e91073eb6c7b5274628be67b539f7362
Douglas Americo <douglas.alex.1212@gmail.com>
2026-03-16 22:40:07 -0300

    calculos matematicos

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello Word");
+const prompt = require("prompt-sync")();
+
+let n1 = Number(prompt("Digite o primeiro número: "));
+let n2 = Number(prompt("Digite o segundo número: "));
+
+console.log(n1 + n2);
+
+
